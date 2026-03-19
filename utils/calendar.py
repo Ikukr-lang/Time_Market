e748261dd16a0c7051106fa29e2f5cd791c71693
Ikukr-lang <profootballfun@yandex.ru>
2026-03-20 01:13:22 +0300

    Delete utils/calendar.py

--- utils/calendar.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timedelta
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-async def generate_calendar(specialist_id: int, month_offset: int = 0):
-    # здесь логика показа дней на 60 дней вперёд + проверка занятости
-    # возвращает InlineKeyboardMarkup с датами
-    pass  # я могу прислать полную версию в следующем сообщении
