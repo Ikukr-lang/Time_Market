15741ef9abdcf1b59ab46b37421dc2cdfcd4a013
Ikukr-lang <profootballfun@yandex.ru>
2026-03-20 00:00:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+YOOKASSA_SHOP_ID = os.getenv("YOOKASSA_SHOP_ID")
+YOOKASSA_SECRET = os.getenv("YOOKASSA_SECRET")
