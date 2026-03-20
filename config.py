52e54bc8d99f81b76b0b3cbd5790238cfac5a6e2
Ikukr-lang <profootballfun@yandex.ru>
2026-03-20 12:31:40 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-# SECRET для проверки initData (произвольный, но длинный)
-WEBAPP_SECRET = os.getenv("WEBAPP_SECRET", "super-secret-string-change-me-1234567890")
-BASE_URL = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME', 'localhost:10000')}"
